277f6feaf7df1846b99d745f3db44bbe93fd608e
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2024-11-12 21:51:27 +0100

    Update macos.rb, remove obsolete commands

--- macos.rb
+++ macos.rb
@@ -1,14 +1,10 @@
-# Tap for additional packages if necessary
-tap "homebrew/cask"           # Required for macOS cask apps like Docker and VS Code
-tap "homebrew/cask-versions"   # For alternative versions of some applications
-
 # Install Git
 brew "git"
 
 # Install Docker Desktop (cask for GUI applications)
-cask "docker"
+brew "homebrew/cask/docker"
 
 # Install Visual Studio Code
-cask "visual-studio-code"
+brew "homebrew/cask/visual-studio-code"
 
-#Use "brew bundle --file=macos.rb" once brew is installed
+# Use "brew bundle --file macos.rb" once brew is installed
